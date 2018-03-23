f519093f8c698ce710e4452261a5ad82317196cd
Carla Brady <cbrady@bstsoftwarecompany.com>
2018-03-23 09:28:31 -0500

    styling/sass

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,13 +10,24 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "./assets/styles";
+  body {
+    background: url('./assets/images/background.jpg') no-repeat center center fixed;
+    background-size: cover;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+    .application {
+      background: none;
+    }
+  }
 </style>
