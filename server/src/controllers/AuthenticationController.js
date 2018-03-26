31885d2f40320281be56b91a5c4829b249e6305c
Carla Brady <cbrady@bstsoftwarecompany.com>
2018-03-26 12:47:41 -0500

    hashing password

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,17 +1,29 @@
 const {User} = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+function jwtSignUser (user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
 
 module.exports = {
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
     } catch (err) {
       res.status(400).send({
         error: 'This email account is already in use.'
       })
     }
   },
-
   async login (req, res) {
     try {
       const {email, password} = req.body
@@ -20,26 +32,28 @@ module.exports = {
           email: email
         }
       })
+
       if (!user) {
         return res.status(403).send({
-          error: 'The login information was incorrect.'
+          error: 'The user information was incorrect'
         })
       }
 
-      const isPasswordValid = password === user.password
+      const isPasswordValid = await user.comparePassword(password)
       if (!isPasswordValid) {
         return res.status(403).send({
-          error: 'The login information was incorrect.'
+          error: 'The password information was incorrect'
         })
       }
 
-      const userJSON = user.toJSON()
+      const userJson = user.toJSON()
       res.send({
-        user: userJSON
+        user: userJson,
+        token: jwtSignUser(userJson)
       })
     } catch (err) {
       res.status(500).send({
-        error: 'An error has occured trying to login.'
+        error: 'An error has occured trying to log in'
       })
     }
   }
