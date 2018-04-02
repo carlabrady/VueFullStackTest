22809286276d372e922e01a826489f50ee50e3db
Carla Brady <cbrady@bstsoftwarecompany.com>
2018-04-02 11:59:43 -0500

    deploy working with route file

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+require('./routes')(app)
+
 app.get('/status', (req, res) => {
   res.sendStatus(200)
 })
 
-app.listen(process.env.port || 8081)
+app.listen(process.env.port || config.port)
---
+++ server/src/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    port: 8081
+    // port: process.env.PORT || 8081,
+    // db: {
+    //   database: process.env.RDS_DB_NAME || 'VueFullStackTest',
+    //   user: process.env.RDS_USERNAME || 'VueFullStackTest',
+    //   password: process.env.RDS_PASSWORD || 'VueFullStackTest',
+    //   options: {
+    //     dialect: process.env.DIALECT || 'mssql',
+    //     host: process.env.RDS_HOSTNAME || 'localhost',
+    //     port: process.env.RDS_PORT,
+    //     storage: './VueFullStackTest'
+    //   }
+    // },
+    // authentication: {
+    //   jwtSecret: process.env.JWT_SECRET || 'secret'
+    // }
+}
