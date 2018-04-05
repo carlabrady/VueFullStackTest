5b0af8275eca3fdf9237243dbc0a998573734d46
Carla Brady <cbrady@bstsoftwarecompany.com>
2018-04-05 10:13:35 -0500

    environment vars

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://serverrebuild-dev.us-west-2.elasticbeanstalk.com/'
+    baseURL: 'http://serverrebuild-dev2.us-west-2.elasticbeanstalk.com/'
   })
 }
